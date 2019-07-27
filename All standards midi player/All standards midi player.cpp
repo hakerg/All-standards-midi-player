b75cbc5862c7f16a5ff14146db97bfd6d0de7151
hakerg <grzegorzabedzki@gmail.com>
2019-07-27 02:31:04 +0200

    added compression and infra filter

--- All standards midi player/All standards midi player.cpp
+++ All standards midi player/All standards midi player.cpp
@@ -12,6 +12,36 @@ HSOUNDFONT soundfonts[3];
 vector<BASS_MIDI_FONTEX> font_info;
 HSTREAM stream;
 string exe_path, resources_path;
+float filterRC = 1.0 / (20.0 * 6.283185307179586476925286766559);
+vector<float> filter_prev_filtered, filter_prev;
+
+void CALLBACK filter20Hz_f(
+	HDSP handle,
+	DWORD channel,
+	void* buffer,
+	DWORD length,
+	void* user)
+{
+	BASS_CHANNELINFO info;
+	BASS_ChannelGetInfo(channel, &info);
+	filter_prev.resize(info.chans, 0.0f);
+	filter_prev_filtered.resize(info.chans, 0.0f);
+	float dt = float(1.0 / info.freq);
+	float alpha = filterRC / (filterRC + dt);
+	float* ptr = (float*)buffer;
+	float* end = (float*)((byte*)(buffer) + length);
+	while (ptr != end)
+	{
+		for (DWORD i = 0; i < info.chans; i++)
+		{
+			float filtered = alpha * (filter_prev_filtered[i] + (*ptr) - filter_prev[i]);
+			filter_prev[i] = *ptr;
+			filter_prev_filtered[i] = filtered;
+			*ptr = filtered;
+			ptr++;
+		}
+	}
+}
 
 int main(int argc, char** argv)
 {
@@ -25,17 +55,17 @@ int main(int argc, char** argv)
 
 	soundfonts[0] = BASS_MIDI_FontInit(string(resources_path + "LSB bank.sf2").c_str(), NULL);
 	cout << BASS_ErrorGetCode() << endl;
-	//BASS_MIDI_FontSetVolume(soundfonts[0], 1.0f);
-	//cout << BASS_ErrorGetCode() << endl;
+	BASS_MIDI_FontSetVolume(soundfonts[0], 1.25f);
+	cout << BASS_ErrorGetCode() << endl;
 
 	soundfonts[1] = BASS_MIDI_FontInit(string(resources_path + "MSB bank.sf2").c_str(), BASS_MIDI_FONT_XGDRUMS);
 	cout << BASS_ErrorGetCode() << endl;
-	//BASS_MIDI_FontSetVolume(soundfonts[1], 1.0f);
+	//BASS_MIDI_FontSetVolume(soundfonts[1], 0.5f);
 	//cout << BASS_ErrorGetCode() << endl;
 
 	soundfonts[2] = BASS_MIDI_FontInit(string(resources_path + "MT-32.sf2").c_str(), NULL);
 	cout << BASS_ErrorGetCode() << endl;
-	//BASS_MIDI_FontSetVolume(soundfonts[2], 1.0f);
+	//BASS_MIDI_FontSetVolume(soundfonts[2], 0.5f);
 	//cout << BASS_ErrorGetCode() << endl;
 
 	BASS_MIDI_FONTEX fontex;
@@ -70,7 +100,10 @@ int main(int argc, char** argv)
 	BASS_MIDI_StreamSetFonts(NULL, font_info.data(), (DWORD)font_info.size() | BASS_MIDI_FONT_EX);
 	cout << BASS_ErrorGetCode() << endl;
 
-	stream = BASS_MIDI_StreamCreateFile(false, argv[1], NULL, NULL, BASS_SAMPLE_LOOP | BASS_MIDI_DECAYEND | BASS_MIDI_DECAYSEEK, NULL);
+	stream = BASS_MIDI_StreamCreateFile(false, argv[1], NULL, NULL,
+		BASS_SAMPLE_LOOP | BASS_MIDI_DECAYEND | BASS_MIDI_DECAYSEEK | BASS_SAMPLE_FLOAT, NULL);
+	cout << BASS_ErrorGetCode() << endl;
+	BASS_ChannelSetDSP(stream, filter20Hz_f, NULL, 0);
 	cout << BASS_ErrorGetCode() << endl;
 	BASS_ChannelPlay(stream, false);
 	cout << BASS_ErrorGetCode() << endl;
